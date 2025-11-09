ddc67f845543938206e12ac5a2bcc916eea20f87
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-11-09 10:46:01 +0800

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+import messagesRoutes from "./routes/messages.route.js";
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+console.log(process.env.PORT)
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messagesRoutes)
+
+app.listen(PORT, () => console.log("Server is running on port 3000"))
