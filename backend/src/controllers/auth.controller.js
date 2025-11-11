c26e524f140cc2768982b159ddfab9898deb1432
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-11-11 10:30:41 +0800

    login & logout functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -35,9 +35,6 @@ export const signup = async (req, res) => {
         })
 
         if (newUser) {
-            // generateToken(newUser._id, res)
-            // await newUser.save()
-
             const savedUser = await newUser.save();
             generateToken(savedUser._id, res)
 
@@ -62,4 +59,33 @@ export const signup = async (req, res) => {
         console.log("Error in singup controller:", error)
         res.status(500).json({ message: "Internal server error" });
     }
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+        if (!user) return res.status(400).json({ message: "Invalid Credentials" })
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid Credentials" })
+
+        generateToken(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.error("Error login controller: ", error)
+        res.sttus(500).json({message: "Intenal server error"})
+    }
+}
+
+export const logout = async (_, res) => {
+    res.cookie("jwt", "", {maxAge: 0})
+    res.status(200).json({ message: "Logged out successfuly"})
 }
