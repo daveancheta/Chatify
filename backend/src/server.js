4c6c3ebfbabe4adc95b65ff21db704f1dc48d35c
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-11-11 11:32:52 +0800

    update profile

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser"
 import dotenv from "dotenv";
 import path from "path";
 
@@ -15,6 +16,8 @@ const __dirname = path.resolve()
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json())
+app.use(cookieParser())
+
 
 console.log(ENV.PORT)
 
