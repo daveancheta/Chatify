21bee5515f83ea42673ec7aa71fc9397658004e0
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-11-17 21:01:14 +0800

    login and logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,11 +1,13 @@
 import { create } from "zustand"
 import { axiosInstance } from "../lib/axios"
 import toast from "react-hot-toast"
+import axios from "axios"
 
 export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
 
     checkAuth: async () => {
         try {
@@ -31,6 +33,32 @@ export const useAuthStore = create((set) => ({
             toast.error(error.response.data.message)
         } finally {
             set({ isSigningUp: false })
+        }
+    },
+
+    login: async (data) => {
+        set({ isSigningUp: true })
+        try {
+            const res = await axiosInstance.post("/auth/login", data)
+            set({ authUser: res.data })
+
+            toast.success("Logged in successfully")
+
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isLoggingIn: false })
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out sucessfully");
+        } catch (error) {
+            toast.error("Error Loggingout");
+            console.log("Logout error", error);
         }
     }
 }))
